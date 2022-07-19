200583ad8994112ac383cb428a553e27a101840a
Admin <Admin@DESKTOP-MNBM418>
2022-07-19 16:15:42 +0530

    # WARNING: head commit changed in the meantime
    hghgjh

---
+++ SELENIUM/src/test/java/selenium/practice1.java
@@ -0,0 +1,12 @@
+package selenium;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class practice1 {
+@Test
+	public void practiceTest() {
+	Reporter.log("hi");	
+	}
+
+}
