c82a3674b80e45b756c3c83f2e5b0a8c1aee214f
Admin <Admin@DESKTOP-MNBM418>
2022-07-20 17:52:15 +0530

    hiiii

---
+++ MavenPractice/src/test/java/MAVENPROJECT/MavenClassTest.java
@@ -0,0 +1,13 @@
+package MAVENPROJECT;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MavenClassTest {
+	@Test
+public void display() {
+		Reporter.log("Hi");
+	
+}
+	
+}
